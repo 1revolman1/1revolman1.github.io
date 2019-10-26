8bb6e7476370a3c72a8850c950dc46d398bec552
revolman <1revolman1@gmail.com>
2019-10-26 15:35:55 +0300

    Add new changes

---
+++ TIL WEB FINAL/index.js
@@ -0,0 +1,6 @@
+document.querySelector(".button-left").addEventListener("click", function(e) {
+  console.log("left");
+});
+document.querySelector(".button-right").addEventListener("click", function(e) {
+  console.log("right");
+});
