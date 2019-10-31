e6329062c2263bf8b928dfada898c441cdcbe1bc
revolman <1revolman1@gmail.com>
2019-11-01 00:11:45 +0200

    Change button concept

--- TIL WEB FINAL/index.js
+++ TIL WEB FINAL/index.js
@@ -155,3 +155,18 @@ document
       }
     }
   });
+document.querySelector(".menu_btn").addEventListener("click", function(e) {
+  if (
+    !(
+      document.querySelector(".menu__box").style.top ==
+      `${document.querySelector("header").offsetHeight - 1}px`
+    )
+  )
+    document.querySelector(".menu__box").style.top = `${document.querySelector(
+      "header"
+    ).offsetHeight - 1}px`;
+  else
+    document.querySelector(".menu__box").style.top = `-${document.querySelector(
+      ".menu__box"
+    ).offsetHeight + 1}px`;
+});
