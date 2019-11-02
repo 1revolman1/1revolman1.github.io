0d9732815e03df1c1726bd1ae5a6660e153ef76b
revolman <1revolman1@gmail.com>
2019-11-03 00:56:07 +0200

    Update header.js function

--- TIL WEB FINAL/header.js
+++ TIL WEB FINAL/header.js
@@ -26,6 +26,7 @@ window.addEventListener("scroll", function() {
 document
   .querySelector(".wrapper-list")
   .addEventListener("mouseover", function(event) {
+    //cleaning background in the .wrapper-list ul childs
     event.srcElement.offsetParent.children[0].childNodes.forEach(function(e) {
       if (!(e.nodeName == "#text")) {
         e.childNodes[0].style.background = "";
@@ -36,14 +37,16 @@ document
     section.className = "hover-menu";
     section.style.top = `${document.querySelector("header").offsetHeight -
       1}px`;
+    //if .hover-menu EXIST - remove him
     if (!!document.querySelector(".hover-menu")) {
-      //
       document.querySelector("header .wrapper").style.background = "";
       document.querySelector("header").style.background = "";
       event.target.style.background = "#33a7db";
       document.querySelector(".hover-menu").remove();
       document.querySelector(".wrapper-trial").style.display = "";
-    } else if (!(event.target.textContent == "Contact")) {
+    }
+    //if li="Contact" - skip. Else - do
+    if (!(event.target.textContent == "Contact")) {
       document.querySelector(".wrapper-trial").style.display = "none";
       if (event.target.textContent == "About") {
         section.innerHTML = `<div class="wrapper"> <div class="elem1"> <div class="img"><img src="${text_location}assets/Grupo 170.svg" alt=""></div> <ul> <li><a href="${text_location}pages/whatistil">What is TIL?</a></li> <li><a href="${text_location}pages/team">Team</a></li> <li><a href="${text_location}pages/jobs">Jobs</a></li> </ul> </div> <div class="elem2"> <div class="img"><img src="${text_location}assets/Grupo 121.svg" alt=""></div> <ul> <li><a href="${text_location}pages/results">Results and Stats</a></li> <li><a href="${text_location}pages/pricing">Pricing</a></li> </ul> </div> <div class="elem3"> <div class="img"><img src="${text_location}assets/Grupo 128.svg" alt=""></div> <ul> <li><a href="${text_location}pages/tilevents">TIL Events</a></li> <li><a href="${text_location}pages/testimonials">Testimonials</a></li> </ul> </div> </div>`;
@@ -52,6 +55,16 @@ document
         document.querySelector("header").style.background = "#9879ED";
         section.style.background = "#7d56e9";
         document.body.append(section);
+        //Cleaning SECTION
+        //Listener, that check relocation from our creating section
+        //if it's true(we are relocating) we remove section
+        section.addEventListener("mouseleave", function(e) {
+          document.querySelector("header .wrapper").style.background = "";
+          document.querySelector("header").style.background = "";
+          event.target.style.background = "#33a7db";
+          document.querySelector(".hover-menu").remove();
+          document.querySelector(".wrapper-trial").style.display = "";
+        });
       } else if (event.target.textContent == "Teachers") {
         section.innerHTML = `<div class="wrapper"> <div class="elem1"> <div class="img"><img src="${text_location}assets/Grupo 173.svg" alt=""></div> <ul> <li><a href="${text_location}pages/whytil">Why TIL?</a></li> <li><a href="${text_location}pages/ourlanguages">Our Languages</a></li> <li><a href="${text_location}pages/ourvideos">Our Videos</a></li> <li><a href="${text_location}pages/ourexercises">Our Exercises</a></li> <li><a href="${text_location}pages/ourcompetitions">Our Competitions</a></li> </ul> </div> <div class="elem2"> <div class="img"><img src="${text_location}assets/Grupo 136.svg" alt=""></div> <ul> <li><a href="${text_location}pages/trainingandpd">Training and PD</a></li> <li><a href="${text_location}pages/textbooks">Textbooks Alignments</a></li> </ul> </div> </div>`;
         section.style.height = "381px";
@@ -63,15 +76,18 @@ document
         section.childNodes[0].childNodes[1].style.margin = "0 15%";
         document.querySelector(".hover-menu .wrapper").style.justifyContent =
           "flex-start";
+        //Cleaning SECTION
+        //Listener, that check relocation from our creating section
+        //if it's true(we are relocating) we remove section
+        section.addEventListener("mouseleave", function(e) {
+          document.querySelector("header .wrapper").style.background = "";
+          document.querySelector("header").style.background = "";
+          event.target.style.background = "#33a7db";
+          document.querySelector(".hover-menu").remove();
+          document.querySelector(".wrapper-trial").style.display = "";
+        });
       }
     }
-    setTimeout(function(e) {
-      document
-        .querySelector(".hover-menu")
-        .addEventListener("click", function(e) {
-          console.log(e);
-        });
-    }, 500);
   });
 
 //This function create link to the header in mobile resolution to .menu__box to make
