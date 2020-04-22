be55ab39c9b83cf8caee81e5c3a2afde54e05517
1revolman1 <1revolman1@gmail.com>
2020-04-22 10:47:04 +0300

    avtoopora

---
+++ Avtoopora/precache-manifest.f529c1169e0d1873040eddd2a12dc4a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0233626268c686c4717049957f1f77c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e695f3882b3c01ce387",
+    "url": "/static/css/main.c23d8a01.chunk.css"
+  },
+  {
+    "revision": "eb543a66f59c948b7df6",
+    "url": "/static/js/2.a5d7ee66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5d7ee66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e695f3882b3c01ce387",
+    "url": "/static/js/main.7b9916dd.chunk.js"
+  },
+  {
+    "revision": "da5cda2080d9a9ca20b1",
+    "url": "/static/js/runtime-main.25d13b19.js"
+  },
+  {
+    "revision": "9024155ea9283d18d0a9540550542b4c",
+    "url": "/static/media/arrow.9024155e.svg"
+  },
+  {
+    "revision": "f0a701821628e1a079d7e1237008a987",
+    "url": "/static/media/logo.f0a70182.svg"
+  },
+  {
+    "revision": "4af1faba95fc93e344e10d3ddd404c00",
+    "url": "/static/media/search.4af1faba.svg"
+  },
+  {
+    "revision": "9620ace451fd96211ec29c206d6bb044",
+    "url": "/static/media/star.9620ace4.svg"
+  },
+  {
+    "revision": "f4942686a8f1919f3d1c2f85a47f7e50",
+    "url": "/static/media/star.f4942686.svg"
+  },
+  {
+    "revision": "93936ec3632f360e31475bd823c7d006",
+    "url": "/static/media/supermarket.93936ec3.svg"
+  }
+]);
