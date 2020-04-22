30735ed8d03ec2dbbe6888c0e724ecb132abbaab
1revolman1 <1revolman1@gmail.com>
2020-04-22 11:34:14 +0300

    add

---
+++ react-build/precache-manifest.2f00b99c08c18459dda920622fb69af9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea58e7c00a1fa8bd6203e81c44930a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fde547582e3c58a4858",
+    "url": "/static/css/main.852d509f.chunk.css"
+  },
+  {
+    "revision": "e426fde4d36e24ceb80f",
+    "url": "/static/js/2.168f61d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.168f61d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fde547582e3c58a4858",
+    "url": "/static/js/main.b8448f45.chunk.js"
+  },
+  {
+    "revision": "2e05826369fee773398e",
+    "url": "/static/js/runtime-main.d5303d8d.js"
+  },
+  {
+    "revision": "9024155ea9283d18d0a9540550542b4c",
+    "url": "/static/media/arrow.9024155e.svg"
+  },
+  {
+    "revision": "f0a701821628e1a079d7e1237008a987",
+    "url": "/static/media/logo.f0a70182.svg"
+  },
+  {
+    "revision": "4af1faba95fc93e344e10d3ddd404c00",
+    "url": "/static/media/search.4af1faba.svg"
+  },
+  {
+    "revision": "9620ace451fd96211ec29c206d6bb044",
+    "url": "/static/media/star.9620ace4.svg"
+  },
+  {
+    "revision": "f4942686a8f1919f3d1c2f85a47f7e50",
+    "url": "/static/media/star.f4942686.svg"
+  },
+  {
+    "revision": "93936ec3632f360e31475bd823c7d006",
+    "url": "/static/media/supermarket.93936ec3.svg"
+  }
+]);
